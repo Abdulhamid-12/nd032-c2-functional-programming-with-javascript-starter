6ba32455bc03bb0412e3e6f15fb91f6bc1b2ed49
Abdulhamid-12 <alabbad.2021@gmail.com>
2022-11-11 18:58:04 +0300

    compare paradigms

--- intro_to_fp/compare_paradigms.js
+++ intro_to_fp/compare_paradigms.js
@@ -1,18 +1,27 @@
-// Directions: Rewrite the imperative code below as Object-Oriented 
+// Directions: Rewrite the imperative code below as Object-Oriented
 
-let status = 'active'
-let warp = 2
-let type = 'Dilithium Crystal'
-let status_report = 'Captain, '
+let status = "active";
+let warp = 2;
+let type = "Dilithium Crystal";
+let status_report = "Captain, ";
 
-if(status === 'active' && warp <= 4) {
-    status_report += 'the engines are active and we could be going faster.'
-} else if (status === 'active' && warp > 4) {
-    status_report += 'the engines are active and we are going ' + warp + '.'
-} else if (status === 'down') {
-    status_report += 'the engines are down.'
+if (status === "active" && warp <= 4) {
+  status_report += "the engines are active and we could be going faster.";
+} else if (status === "active" && warp > 4) {
+  status_report += "the engines are active and we are going " + warp + ".";
+} else if (status === "down") {
+  status_report += "the engines are down.";
 } else {
-    status_report += 'the comms are down and we can`t reach engineering.'
+  status_report += "the comms are down and we can`t reach engineering.";
 }
 
-console.log(status_report)
+console.log(status_report);
+
+class Report {
+  constructor(status, warp, type, status_report) {
+    this.status = status;
+    this.warp = warp;
+    this.type = type;
+    this.status_report = status_report;
+  }
+}
